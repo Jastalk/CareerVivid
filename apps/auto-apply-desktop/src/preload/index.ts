d78db3ca3769bcad31660999c06388d7f2002fef
JiawenZhu <zhujiawen519@gmail.com>
2026-08-30 12:29:24 -0500

    chore: untrack desktop app and update gitignore

--- apps/auto-apply-desktop/src/preload/index.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('autoApplyRunner', {
-  getStatus: () => ipcRenderer.invoke('runner:get-status'),
-  openCareerVivid: () => ipcRenderer.invoke('runner:open-careervivid'),
-  start: () => ipcRenderer.invoke('runner:start'),
-  stop: () => ipcRenderer.invoke('runner:stop'),
-});
